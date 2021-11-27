c0570373998fb8aa9da66f0b294e2293b7dbec84
Engku Ihsan <eihsan94@gmail.com>
2021-11-27 14:32:10 +0900

    project starts complete

---
+++ components/SeoHeader.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import React from 'react'
+
+const SeoHeader: React.FC = ({children}) =>  {
+    const title = 'Como Sedice | スペイン語勉強'
+    const description = 'スペイン語を簡単に勉強できるところ'
+    const tags = ['スペイン語', '勉強', '簡単', '楽しい', '勉強しやすい']
+
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=0" />
+            <meta name="description" content={description} />
+            <meta name="keywords" content={`${tags.map(t => t)}`} />
+            <meta name="og:title" property="og:title" content={title}  />
+            <meta name="og:type" property="og:type" content="website"  />
+            <meta name="og:site_name" property="og:site_name" content={title}  />
+            <meta name="og:url" property="og:url" content="https://ihsan-pay.vercel.app/"  />
+            <meta name="og:image" property="og:image" content="/images/logo.png"  />
+            <meta name="og:description" property="og:description" content={description}  />
+            <meta name="twitter:card" property="twitter:card" content="summary_large_image"  />
+            <meta name="twitter:title" property="twitter:title" content={title}  />
+            <meta name="twitter:site" property="twitter:site" content="https://ihsan-pay.vercel.app/"  />
+            <meta name="twitter:description" property="twitter:description" content={description}  />
+            <meta name="twitter:creator" property="twitter:creator" content="engku.ihsan"  />
+            <meta name="twitter:image" property="twitter:creator" content="/images/logo.png"  />
+
+            <link rel="icon" type="image/png" href="/favicon.ico" />
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="icon" type="image/png" href="/assets/icons/16.png" sizes= "16x16" />
+            <link rel="icon" type="image/png" href="/assets/icons/32.png" sizes= "32x32" />
+            <link rel="apple-touch-icon" href="/assets/icons/152.png" />
+
+            {children}
+        </Head>
+    )
+}
+
+export default SeoHeader
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import SeoHeader from '../components/SeoHeader'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <SeoHeader  />
+      <Component {...pageProps} />
+    </ChakraProvider>
+
+  )
 }
 
 export default MyApp
